df45747cc8a3209271bc4e32de9a1a03325251aa
Unknown <scott_laskowski@live.com>
2018-06-21 21:03:51 -0600

    Have a command working
    Have basic info required for uptime maybe.

--- app.js
+++ app.js
@@ -1,10 +1,76 @@
+const querystring = require("querystring"),
+    fetch = require("node-fetch");
+
 //file io module
 const fs = require('fs');
+
+//Commands
+var commands = [];
+
+const client_ID = "wy7ihv9surttczc5loyso4pbfcdf96";
+const streams_URL = "https://api.twitch.tv/helix/streams";
+
+const qs = querystring.stringify({
+	first: 1,
+	user_id : 47207841
+});
+
+const qUrl = `${streams_URL}?${qs}`;
+
+const fetchArgs = {
+    headers: {
+        "Client-ID": client_ID
+    }
+};
+
+fetch(qUrl, fetchArgs)
+    .then(res => res.json())
+    .then(data => uptimeStart = data.started_at)
+    .catch(err => console.error(err));
+
+
+
+
+function Command(name, call, private){
+	this.identifier = name;
+	this.call = call;
+	this.global = private;
+};
+
+function addCommand(name, call, private){
+	if(commandExists(name)){
+		return;
+	}
+	commands.push(new Command(name, call, private));
+	saveCommands();
+}
+
+function commandExists(name){
+	for(var i=0;i<commands.length;i++){
+		if (commands[i].name == name){
+			return true;
+		}
+	}
+	return false;
+}
+
+function saveCommands(){
+	fs.writeFile('commands.txt', JSON.stringify(commands), (error) => {if(error != null){console.log(error)}});
+	return;
+}
+
+function loadCommands(){
+	fs.readFile('commands.txt', (err,data) => {
+		if (err) console.log(err);
+		commands = JSON.parse(data);
+	});
+}
+
 
 //twitch chat api module
 var tmi = require('tmi.js');
 
-var commands = [];
+var emotes = [];
 
 var options = {
 	options: {
@@ -23,8 +89,36 @@ var options = {
 
 var client = new tmi.client(options);
 client.connect();
+addCommand('Hiya', function(){
+	client.say("skiskow","Hiya I'm a bot!");
+}
+, false);
+// loadCommands();
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
 
 client.on('chat', function(channel, userstate, message, self) {
 	if(self) return;
-	fs.appendFile(channel + "log.txt", '\n' + message, (error) => {if(error != null){console.log(error)}});
-})
+	if(message.startsWith('!')){
+		if(userstate.mod == true || userstate.username == channel.substring(1)){
+			switch(message.substring(1).toLowerCase()){
+				case 'hiya':
+					client.say("skiskow","Hiya I'm a bot!");
+					break;
+				case 'uptime':
+					client.say("skiskow", "Uptime is hard give me some time to learn how to do it.");
+					break;
+				// case 'template':
+				// 	client.say("skiskow", "message");
+				// 	break;
+			}
+		}	
+	}
+	fs.appendFile(channel  + dd + mm + yyyy + "log.txt", '\n' + userstate.username + ": " + message, (error) => {if(error != null){console.log(error)}});
+	if(message.includes("?")){
+		fs.appendFile(channel  + dd + mm + yyyy + "Questionlog.txt", '\n' + userstate.username + ": " + message, (error) => {if(error != null){console.log(error)}});
+	}
+});
